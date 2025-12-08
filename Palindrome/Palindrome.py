500252c2b5d3e3e74f3950932b9385d3b50f72d5
emiliaMetelitsa <klthsrswm@gmail.com>
2025-12-08 21:32:23 +0300

    Update Palindrome.py

--- Palindrome/Palindrome.py
+++ Palindrome/Palindrome.py
@@ -1,15 +1,16 @@
-def is_palindrome(text: str) -> bool:
+def is_palindrome(string: str) -> bool:
     """
     Проверяет, является ли строка палиндромом.
-
-    :param text: Входная строка
-    :return: True, если палиндром, иначе False
+    :param string: проверяемая строка
+    :return: True, если строка палиндром, иначе False
     """
-    for i in range(len(text)):
-        if text[i] != text[-(i + 1)]:
+    for i in range(len(string)):
+        if string[i] != string[-(i + 1)]:
             return False
     return True
 
-"""Пример использования"""
-user_input = input("Введите строку: ")
-print(is_palindrome(user_input))
+
+if __name__ == "__main__":
+    # Пример использования
+    user_input = input("Введите строку: ")
+    print(is_palindrome(user_input))
